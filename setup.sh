db971eb79b5397a4043c92206db11ff75f92ab2f
Chyld Medford <chyld.medford@gmail.com>
2016-06-27 00:24:10 -0500

    ready

---
+++ setup.sh
@@ -0,0 +1,13 @@
+npm install react \
+            react-dom --save
+
+npm install babel-core \
+            babel-loader \
+            babel-preset-es2015 \
+            babel-preset-react \
+            babel-preset-stage-0 \
+            babel-register \
+            express \
+            react-hot-loader \
+            webpack \
+            webpack-dev-server --save-dev
